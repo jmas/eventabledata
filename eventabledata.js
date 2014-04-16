9f132c36a721e4e5719f2e7c693882655e54a6cd
Alexander Maslakov <alexandr@ego-cms.com>
2014-04-16 22:22:16 +0300

    Files.

---
+++ eventabledata.js
@@ -0,0 +1,23 @@
+define('eventable', function() {
+  var EventableData = function(o) { this.o = o; };
+
+  eventable(EventableData.prototype);
+
+  EventableData.prototype.o = null;
+
+  EventableData.prototype.toJSON = function() {
+    return this.o;  
+  };
+
+  EventableData.prototype.set = function(key, value) {
+    this.o[key] = value;
+    this.fire('set', [key, value]);
+  };
+
+  EventableData.prototype.get = function(key) {
+    this.fire('get', [key, this.o[key] || null]);
+    return this.o[key] || null;
+  };
+
+  return EventableData;
+});
